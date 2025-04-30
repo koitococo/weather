48425fd1cd3f7f59b1a202a42cf1dcfe1a7bdcbd
Koi to Coco <koitococo@outlook.com>
2025-04-30 12:59:06 +0800

    refine styles

--- src/components/SunCard.tsx
+++ src/components/SunCard.tsx
@@ -1,37 +1,38 @@
-import { DailyAstroType } from "@/types/daily";
-import DataCard, { DataCardProps } from "@/components/DataCard";
+import type { DailyAstroType } from "@/types/daily";
+import DataCard, { type DataCardProps } from "@/components/DataCard";
 import { Sunrise, Sunset, Sunset2 } from "tabler-icons-react";
 import DataItems from "@/components/DataItems";
 
-export interface SunCardProps extends Omit<DataCardProps, "icon" | "title"> {
+export default function SunCard({
+  data,
+  ...props
+}: Omit<DataCardProps, "icon" | "title"> & {
   data?: DailyAstroType;
-}
-
-export default function SunCard({ data, ...props }: SunCardProps) {
+}) {
   return (
     <DataCard
       {...props}
       icon={<Sunset2 size={14} />}
       title="日出日落">
       <div className="h-full flex justify-center items-center">
-        <div className="flex flex-col grow w-full h-full justify-between items-center max-w-[200px] ">
-        <DataItems
-          data={[
-            {
-              icon: <Sunrise size={16} />,
-              title: "日出",
-              value: data?.sunrise?.time,
-            },
-            {
-              icon: <Sunset size={16} />,
-              title: "日落",
-              value: data?.sunset?.time,
-            },
-          ]}
-          spacing={"xs"}
-          fallback="--:--"
-          align="center"
-        />
+        <div className="flex flex-col grow w-full justify-between items-center max-w-[200px] ">
+          <DataItems
+            data={[
+              {
+                icon: <Sunrise size={16} />,
+                title: "日出",
+                value: data?.sunrise?.time,
+              },
+              {
+                icon: <Sunset size={16} />,
+                title: "日落",
+                value: data?.sunset?.time,
+              },
+            ]}
+            spacing={"xs"}
+            fallback="--:--"
+            align="center"
+          />
         </div>
       </div>
     </DataCard>
